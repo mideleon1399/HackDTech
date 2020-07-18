02d194a2e569e552a66b48c7d7232495373f0f7d
Bailey Heit <bailey.heit@duke.edu>
2020-07-18 09:42:31 -0700

    hello

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -11,7 +11,7 @@ function App() {
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <p>
-          Hello Bailey
+          Hello Mia
         </p>
         <a
           className="App-link"
