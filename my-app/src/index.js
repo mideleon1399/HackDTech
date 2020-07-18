f4450e884ea53f0470137e8d04eb33a8740cb961
Mia de Leon <mia.de.leon@duke.edu>
2020-07-18 14:53:18 -0400

    navbar and other pages

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,19 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
-
-import "./index.css";
-
-import MapChart from "./MapChart";
-
-function App() {
-  return (
-    <>
-    <div>
-      <MapChart />
-    </div>
-    </>
-  );
-}
-
-const rootElement = document.getElementById("root");
-ReactDOM.render(<App />, rootElement);
+import { BrowserRouter} from 'react-router-dom';
+import App from './App'
+ReactDOM.render(
+  <BrowserRouter>
+      <App />
+  </BrowserRouter>, 
+  document.getElementById('root')
+)
---
+++ my-app/src/intro.js
@@ -0,0 +1,13 @@
+import React from "react";
+import './index.css';
+
+class Intro extends React.Component {
+    render() {
+      return (  
+          <div>
+            intro
+          </div>
+      );
+    }
+  }
+export default Intro; 
