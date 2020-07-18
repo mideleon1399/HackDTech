0692f6808ac8c1289f0789f4055dc4fe9c7d567e
Mia de Leon <mia.de.leon@duke.edu>
2020-07-18 16:30:02 -0400

    nav

--- my-app/src/MapChart.js
+++ my-app/src/MapChart.js
@@ -1,21 +1,14 @@
 import React, { useState, useEffect } from "react";
 import { ComposableMap, Geographies, Geography } from "react-simple-maps";
-import { scaleQuantize } from "d3-scale";
+import { scaleLinear } from "d3-scale";
 import { csv } from "d3-fetch";
 
 const geoUrl = "https://cdn.jsdelivr.net/npm/us-atlas@3/counties-10m.json";
 
-const colorScale = scaleQuantize()
-  .domain([1, 10])
+const colorScale = scaleLinear()
+  .domain([0,1])
   .range([
     "#ffedea",
-    "#ffcec5",
-    "#ffad9f",
-    "#ff8a75",
-    "#ff5533",
-    "#e2492d",
-    "#be3d26",
-    "#9a311f",
     "#782618"
   ]);
 
--- my-app/src/NavBar.js
+++ my-app/src/NavBar.js
@@ -5,9 +5,9 @@ import {Link } from 'react-router-dom';
 function NavBar() {
   return (
     <ul>
-     <li className="link"><Link to="/" >What is COVID-19 </Link></li>
-     <li className="link"> <Link to="/map" className="link">Map </Link></li>
-     <li className="link"> <Link to="/resources" className="link"> Resources </Link></li>
+     <li className="link"><Link to="/" ><div className="button">What is COVID-19 </div></Link></li>
+     <li className="link"> <Link to="/map" ><div className="button">Map </div></Link></li>
+     <li className="link"> <Link to="/resources" ><div className="button">Resources </div></Link></li>
     </ul>
   );
 };
