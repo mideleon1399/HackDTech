6860d4dc9b09ea9d32fc93da9422583829cc724d
Mia de Leon <mia.de.leon@duke.edu>
2020-07-18 13:38:07 -0400

    coronavirus data

--- my-app/src/MapChart.js
+++ my-app/src/MapChart.js
@@ -1,56 +1,53 @@
-import React, { useEffect, useState } from "react";
+import React, { useState, useEffect } from "react";
+import { ComposableMap, Geographies, Geography } from "react-simple-maps";
+import { scaleQuantize } from "d3-scale";
 import { csv } from "d3-fetch";
-import { scaleLinear } from "d3-scale";
-import {
-  ComposableMap,
-  Geographies,
-  Geography,
-  Sphere,
-  Graticule
-} from "react-simple-maps";
 
-const geoUrl =
-  "https://raw.githubusercontent.com/zcreativelabs/react-simple-maps/master/topojson-maps/world-110m.json";
+const geoUrl = "https://cdn.jsdelivr.net/npm/us-atlas@3/counties-10m.json";
 
-const colorScale = scaleLinear()
-  .domain([0.29, 0.68])
-  .range(["#ffedea", "#ff5233"]);
+const colorScale = scaleQuantize()
+  .domain([1, 10])
+  .range([
+    "#ffedea",
+    "#ffcec5",
+    "#ffad9f",
+    "#ff8a75",
+    "#ff5533",
+    "#e2492d",
+    "#be3d26",
+    "#9a311f",
+    "#782618"
+  ]);
 
 const MapChart = () => {
   const [data, setData] = useState([]);
 
   useEffect(() => {
-    csv(`/vulnerability.csv`).then(data => {
-      setData(data);
+    // https://www.bls.gov/lau/
+    csv("/unemployment-by-county-2017.csv").then(counties => {
+      setData(counties);
     });
   }, []);
 
   return (
-    <ComposableMap
-      projectionConfig={{
-        rotate: [-10, 0, 0],
-        scale: 147
-      }}
-    >
-      <Sphere stroke="#E4E5E6" strokeWidth={0.5} />
-      <Graticule stroke="#E4E5E6" strokeWidth={0.5} />
-      {data.length > 0 && (
+    <>
+      <ComposableMap projection="geoAlbersUsa">
         <Geographies geography={geoUrl}>
           {({ geographies }) =>
             geographies.map(geo => {
-              const d = data.find(s => s.ISO3 === geo.properties.ISO_A3);
+              const cur = data.find(s => s.id === geo.id);
               return (
                 <Geography
                   key={geo.rsmKey}
                   geography={geo}
-                  fill={d ? colorScale(d["2017"]) : "#F5F4F6"}
+                  fill={colorScale(cur ? cur.unemployment_rate : "#EEE")}
                 />
               );
             })
           }
         </Geographies>
-      )}
-    </ComposableMap>
+      </ComposableMap>
+    </>
   );
 };
 
