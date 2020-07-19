8a660885af6330a745824b47e629aa9fd3c5edab
Bailey Heit <bailey.heit@duke.edu>
2020-07-18 22:53:15 -0700

    resource links

--- my-app/src/intro.js
+++ my-app/src/intro.js
@@ -5,7 +5,7 @@ class Intro extends React.Component {
     render() {
       return (  
           <div>
-            intro
+            Hi kids! If you have questions about what "Covid-19" is and to stay safe, you've come to the right place! We are here to teach you and share resources so that you stay safe and have fun!
           </div>
       );
     }
--- my-app/src/resources.js
+++ my-app/src/resources.js
@@ -4,8 +4,32 @@ import './index.css';
 class Resources extends React.Component {
     render() {
       return (  
+        
           <div>
-            resources
+            <a 
+            href="https://youtu.be/5OytROGeNt8" target="_blank">DIY MASK
+            </a>
+            <br></br>
+            <a 
+            href="https://youtu.be/OPsY-jLqaXM" target="_blank">COVID for Kids!
+            </a>
+            <br></br>
+            <a 
+            href="https://www.zerotothree.org/resources/3210-tips-for-families-coronavirus" target="_blank">Tips for Families with Young Kids
+            </a>
+            <a 
+            href="https://www.unicef.org/coronavirus/kids-video-diaries-about-life-during-covid-19" target="_blank">Stories from kids our age about Covid!
+            </a>
+            <br></br>
+            <a 
+            href="https://youtu.be/NoxdS4eXy18" target="_blank">Washing your hands the right way!
+            </a>
+            <br></br>
+            <a 
+            href="https://rochester.kidsoutandabout.com/content/250-creative-ways-keep-your-family-sane-during-covid-19-crisis" target="_blank">Crafts for Covid!
+            </a>
+            <br></br>
+            
           </div>
       );
     }
